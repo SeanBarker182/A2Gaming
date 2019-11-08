ef1ab6a30e807312744608796f529a1d85f89fbe
Sean Barker <imseanbarker@gmail.com>
2019-11-08 17:36:47 -0500

    -Finished the gAPI  processing

--- events.js
+++ events.js
@@ -1,185 +1,115 @@
-var clientId =
-  "278666276805-k45o29icg2j6b1a8la4fddonvbaba4jj.apps.googleusercontent.com"; //choose web app client Id, redirect URI and Javascript origin set to http://localhost
-var apiKey = "AIzaSyAOD2s7_35TG9z_Xxu_cAE1oS6wNzLnZso"; //choose public apiKey, any IP allowed (leave blank the allowed IP boxes in Google Dev Console)
-var userEmail = "a2ftwayne@gmail.com"; //your calendar Id
-var userTimeZone = "Indianapolis"; //example "Rome" "Los_Angeles" ecc...
-var maxRows = 10; //events to shown
-var calName = "A2"; //name of calendar (write what you want, doesn't matter)
+/* This solution makes use of "simple access" to google, providing only an API Key.
+ * This way we can only get access to public calendars. To access a private calendar,
+ * we would need to use OAuth 2.0 access.
+ *
+ * "Simple" vs. "Authorized" access: https://developers.google.com/api-client-library/javascript/features/authentication
+ * Examples of "simple" vs OAuth 2.0 access: https://developers.google.com/api-client-library/javascript/samples/samples#authorizing-and-making-authorized-requests
+ *
+ * We will make use of "Option 1: Load the API discovery document, then assemble the request."
+ * as described in https://developers.google.com/api-client-library/javascript/start/start-js
+ */
+function printCalendar() {
+  // The "Calendar ID" from your calendar settings page, "Calendar Integration" secion:
+  var calendarId = "a2ftwayne@gmail.com";
 
-var scopes = "https://www.googleapis.com/auth/calendar";
+  // 1. Create a project using google's wizzard: https://console.developers.google.com/start/api?id=calendar
+  // 2. Create credentials:
+  //    a) Go to https://console.cloud.google.com/apis/credentials
+  //    b) Create Credentials / API key
+  //    c) Since your key will be called from any of your users' browsers, set "Application restrictions" to "None",
+  //       leave "Website restrictions" blank; you may optionally set "API restrictions" to "Google Calendar API"
+  var apiKey = "AIzaSyAOD2s7_35TG9z_Xxu_cAE1oS6wNzLnZso";
+  // You can get a list of time zones from here: http://www.timezoneconverter.com/cgi-bin/zonehelp
+  var userTimeZone = "Europe/Budapest";
 
-//--------------------- Add a 0 to numbers
-function padNum(num) {
-  if (num <= 9) {
-    return "0" + num;
-  }
-  return num;
-}
-//--------------------- end
-
-//--------------------- From 24h to Am/Pm
-function AmPm(num) {
-  if (num <= 12) {
-    return "am " + num;
-  }
-  return "pm " + padNum(num - 12);
-}
-//--------------------- end
-
-//--------------------- num Month to String
-function monthString(num) {
-  if (num === "01") {
-    return "JAN";
-  } else if (num === "02") {
-    return "FEB";
-  } else if (num === "03") {
-    return "MAR";
-  } else if (num === "04") {
-    return "APR";
-  } else if (num === "05") {
-    return "MAJ";
-  } else if (num === "06") {
-    return "JUN";
-  } else if (num === "07") {
-    return "JUL";
-  } else if (num === "08") {
-    return "AUG";
-  } else if (num === "09") {
-    return "SEP";
-  } else if (num === "10") {
-    return "OCT";
-  } else if (num === "11") {
-    return "NOV";
-  } else if (num === "12") {
-    return "DEC";
-  }
-}
-//--------------------- end
-
-//--------------------- from num to day of week
-function dayString(num) {
-  if (num == "1") {
-    return "mon";
-  } else if (num == "2") {
-    return "tue";
-  } else if (num == "3") {
-    return "wed";
-  } else if (num == "4") {
-    return "thu";
-  } else if (num == "5") {
-    return "fri";
-  } else if (num == "6") {
-    return "sat";
-  } else if (num == "0") {
-    return "sun";
-  }
-}
-//--------------------- end
-
-//--------------------- client CALL
-function handleClientLoad() {
-  gapi.client.setApiKey(apiKey);
-  checkAuth();
-}
-//--------------------- end
-
-//--------------------- check Auth
-function checkAuth() {
-  gapi.auth.authorize(
-    { client_id: clientId, scope: scopes, immediate: true },
-    handleAuthResult
-  );
-}
-//--------------------- end
-
-//--------------------- handle result and make CALL
-function handleAuthResult(authResult) {
-  if (authResult) {
-    makeApiCall();
-  }
-}
-
-function makeApiCall() {
-  var today = new Date(); //today date
-  gapi.client.load("calendar", "v3", function() {
-    var request = gapi.client.calendar.events.list({
-      calendarId: userEmail,
-      timeZone: userTimeZone,
-      singleEvents: true,
-      timeMin: today.toISOString(), //gathers only events not happened yet
-      maxResults: maxRows,
-      orderBy: "startTime"
-    });
-    request.execute(function(resp) {
-      for (var i = 0; i < resp.items.length; i++) {
-        var li = document.createElement("li");
-        var item = resp.items[i];
-        var classes = [];
-        var allDay = item.start.date ? true : false;
-        var startDT = allDay ? item.start.date : item.start.dateTime;
-        var dateTime = startDT.split("T"); //split date from time
-        var date = dateTime[0].split("-"); //split yyyy mm dd
-        var startYear = date[0];
-        var startMonth = monthString(date[1]);
-        var startDay = date[2];
-        var startDateISO = new Date(
-          startMonth + " " + startDay + ", " + startYear + " 00:00:00"
-        );
-        var startDayWeek = dayString(startDateISO.getDay());
-        if (allDay == true) {
-          //change this to match your needs
-          var str = [
-            '<font size="4" face="courier">',
-            startDayWeek,
-            " ",
-            startMonth,
-            " ",
-            startDay,
-            " ",
-            startYear,
-            '</font><font size="5" face="courier"> @ ',
-            item.summary,
-            "</font><br><br>"
-          ];
-        } else {
-          var time = dateTime[1].split(":"); //split hh ss etc...
-          var startHour = AmPm(time[0]);
-          var startMin = time[1];
-          var str = [
-            //change this to match your needs
-            '<font size="4" face="courier">',
-            startDayWeek,
-            " ",
-            startMonth,
-            " ",
-            startDay,
-            " ",
-            startYear,
-            " - ",
-            startHour,
-            ":",
-            startMin,
-            '</font><font size="5" face="courier"> @ ',
-            item.summary,
-            "</font><br><br>"
-          ];
+  // Initializes the client with the API key and the Translate API.
+  gapi.client
+    .init({
+      apiKey: apiKey,
+      // Discovery docs docs: https://developers.google.com/api-client-library/javascript/features/discovery
+      discoveryDocs: [
+        "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
+      ]
+    })
+    .then(function() {
+      // Use Google's "apis-explorer" for research: https://developers.google.com/apis-explorer/#s/calendar/v3/
+      // Events: list API docs: https://developers.google.com/calendar/v3/reference/events/list
+      return gapi.client.calendar.events.list({
+        calendarId: calendarId,
+        timeZone: userTimeZone,
+        singleEvents: true,
+        timeMin: new Date().toISOString(), //gathers only events not happened yet
+        maxResults: 5,
+        orderBy: "startTime"
+      });
+    })
+    .then(
+      function(response) {
+        if (response.result.items) {
+          var i = 1;
+          var calendarRows = [];
+          response.result.items.forEach(function(entry) {
+            var dateAt = moment(entry.start.dateTime)
+              .format("L")
+              .split("/");
+            if (dateAt[0] == 01) {
+              monNam = "JAN";
+            } else if (dateAt[0] == 02) {
+              monNam = "FEB";
+            } else if (dateAt[0] == 03) {
+              monNam = "MAR";
+            } else if (dateAt[0] == 04) {
+              monNam = "APR";
+            } else if (dateAt[0] == 05) {
+              monNam = "MAY";
+            } else if (dateAt[0] == 06) {
+              monNam = "JUN";
+            } else if (dateAt[0] == 07) {
+              monNam = "JUL";
+            } else if (dateAt[0] == 08) {
+              monNam = "AUG";
+            } else if (dateAt[0] == 09) {
+              monNam = "SEP";
+            } else if (dateAt[0] == 10) {
+              monNam = "OCT";
+            } else if (dateAt[0] == 11) {
+              monNam = "NOV";
+            } else if (dateAt[0] == 12) {
+              monNam = "DEC";
+            } else {
+              monNam = "ERROR";
+            }
+            var startsAt = moment(entry.start.dateTime).format("LT");
+            var endsAt = moment(entry.end.dateTime).format("LT");
+            calendarRows.push(
+              `<h3 class="event-box">
+          <a id="date${i}" class="evt-mon">${monNam}</a
+          ><a id="title${i}" class="evt-title">${entry.summary}</a><br /><a
+            class="even-date"
+            >${dateAt[1]}</a
+          ><a id="time${i}" class="evt-time">${startsAt} - ${endsAt}</a>
+        </h3>
+      </div>`
+            );
+            i = i + 1;
+          });
+          $("#events").html(calendarRows.join(""));
         }
-        li.innerHTML = str.join("");
-        li.setAttribute("class", classes.join(" "));
-        document.getElementById("events").appendChild(li);
+      },
+      function(reason) {
+        console.log("Error: " + reason.result.error.message);
       }
-      document.getElementById("updated").innerHTML = "updated " + today;
-      document.getElementById("calendar").innerHTML = calName;
-      console.log(str);
-    });
-  });
+    );
 }
+
+// Loads the JavaScript client library and invokes `start` afterwards.
+gapi.load("client", printCalendar);
 
 if (window.addEventListener) {
   //call init() on page load
   console.log("> Adding TC39 Event Listener...");
-  window.addEventListener("load", makeApiCall, false);
+  window.addEventListener("load", printCalendar, false);
 } else if (window.attachEvent) {
   console.log("> Adding MS Event Listener...");
-  window.attachEvent("onload", makeApiCall);
+  window.attachEvent("onload", printCalendar);
 }
