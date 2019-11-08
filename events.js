6d03cf0a752de17440952226374827e3bbadee44
Sean Barker <imseanbarker@gmail.com>
2019-11-08 11:25:42 -0500

    -change

--- events.js
+++ events.js
@@ -175,10 +175,10 @@ function makeApiCall() {
 }
 
 if (window.addEventListener) {
-    //call init() on page load
-    console.log("> Adding TC39 Event Listener...");
-    window.addEventListener("load", makeApiCall, false);
-  } else if (window.attachEvent) {
-    console.log("> Adding MS Event Listener...");
-    window.attachEvent("onload", makeApiCall;
-  }
+  //call init() on page load
+  console.log("> Adding TC39 Event Listener...");
+  window.addEventListener("load", makeApiCall, false);
+} else if (window.attachEvent) {
+  console.log("> Adding MS Event Listener...");
+  window.attachEvent("onload", makeApiCall);
+}
